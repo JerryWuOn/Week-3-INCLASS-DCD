fd114fb6f6992ec96c1d6f2f6228ba89d2d738ca
JerryWuOn <Jon4@my.bcit.ca>
2023-01-23 15:49:37 -0800

    stylizing

---
+++ .history/components/Card/index_20230123145654.js
@@ -0,0 +1,18 @@
+export default function Card({
+    degree = "none",
+    color = "#FBFFB9",
+    font = "0",
+    image = "icon/personTwo.png"
+
+}) {
+    return(
+        <div style={{backgroundColor: color, fontSize: font}}>
+            {
+                color === "red" ? <img src={'icon/business.jpeg'} width="50"/> :
+                color === "blue" ? <img src={'icon/'} width="50"/> :
+                                    <img src={image} width="50"/> 
+            }
+            {degree}
+        </div>
+    )
+}
---
+++ .history/components/Card/index_20230123150034.js
@@ -0,0 +1,18 @@
+export default function Card({
+    degree = "none",
+    color = "#FBFFB9",
+    font = "0",
+    image = "icon/computing..png"
+
+}) {
+    return(
+        <div style={{backgroundColor: color, fontSize: font}}>
+            {
+                color === "red" ? <img src={'icon/person.png'} width="50"/> :
+                color === "blue" ? <img src={'icon/person.png'} width="50"/> :
+                                    <img src={image} width="50"/> 
+            }
+            {degree}
+        </div>
+    )
+}
